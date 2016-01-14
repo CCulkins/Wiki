b536a65b27627047ab06691ef50daac7db18fbb7
Cassandra Culkins and David Remington <cassy.culkins@gmail.com>
2016-01-14 11:52:54 -0800

    add styling, and additional pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-  var pages = [
+  var pages1 = [
     ["pages/arrays.html", "Arrays"],
     ["pages/attributes.html", "Attributes"],
     ["pages/block-elements.html", "Block Elements"],
@@ -8,6 +8,8 @@ $(function(){
     ["pages/branching.html", "Branching"],
     ["pages/cascading.html", "Cascading"],
     ["pages/css.html", "CSS"],
+  ];
+  var pages2 = [
     ["pages/divsandspans.html", "Divs & Spans"],
     ["pages/dom.html", "DOM"],
     ["pages/floats.html", "Floats"],
@@ -16,6 +18,8 @@ $(function(){
     ["pages/inline-elements.html", "Inline Elements"],
     ["pages/javascript-arithmetic.html", "JS Arithmetic"],
     ["pages/jquery.html", "jQuery"],
+  ];
+  var pages3 = [
     ["pages/js-functions.html", "JS Functions"],
     ["pages/js-methods.html", "JS Methods"],
     ["pages/js-strings.html", "JS Strings"],
@@ -23,12 +27,19 @@ $(function(){
     ["pages/looping.html", "Looping"],
     ["pages/markdown.html", "Markdown"],
     ["pages/responsive-design.html", "Responsive Design"]
-];
+  ];
 
 
-  pages.forEach(function(page) {
-    $("#myLinks").append( "<li>" + "<a href=\"" + page[0] + "\">" + page[1] +"<a>" +  "</li>");
+  pages1.forEach(function(page) {
+    $("#mylinks1").append( "<li>" + "<a href=\"" + page[0] + "\">" + page[1] +"<a>" +  "</li>");
+  });
 
+  pages2.forEach(function(page) {
+    $("#mylinks2").append( "<li>" + "<a href=\"" + page[0] + "\">" + page[1] +"<a>" +  "</li>");
+  });
+
+  pages3.forEach(function(page) {
+      $("#mylinks3").append( "<li>" + "<a href=\"" + page[0] + "\">" + page[1] +"<a>" +  "</li>");
   });
 
   });
